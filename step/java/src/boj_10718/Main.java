89206caf509a28be8f2d5b1b41f2abb0fb1a1028
yuyeon329 <peachberryjr@gmail.com>
2021-08-02 21:02:03 +0900

    BOJ 10718.java add

---
+++ step/java/src/boj_10718/Main.java
@@ -0,0 +1,9 @@
+package boj_10718;
+
+public class Main {
+    public static void main(String[] args) {
+        for(int i = 0; i<2; i++){
+            System.out.println("강한친구 대한육군");
+        }
+    }
+}
