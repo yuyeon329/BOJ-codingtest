1a07e071b502345bfb1eff734aecc314ae84b6c7
yuyeon329 <peachberryjr@gmail.com>
2021-08-07 13:31:27 +0900

    BOJ 1008.java add

---
+++ step/java/src/boj_1008/Main.java
@@ -0,0 +1,13 @@
+package boj_1008;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double a,b;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextDouble();
+        b = sc.nextDouble();
+        System.out.println(a/b);
+    }
+}
