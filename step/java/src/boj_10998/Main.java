fb91829dd826771c5756ef09eac1e3f9481c0135
yuyeon329 <peachberryjr@gmail.com>
2021-08-07 12:49:31 +0900

    BOJ 10998.java add

---
+++ step/java/src/boj_10998/Main.java
@@ -0,0 +1,13 @@
+package boj_10998;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a,b;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println(a*b);
+    }
+}
