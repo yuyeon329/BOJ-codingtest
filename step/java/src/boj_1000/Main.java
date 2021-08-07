2b9e9a5f407ca10a96c3eb612f60a01e14709792
yuyeon329 <peachberryjr@gmail.com>
2021-08-07 12:42:12 +0900

    BOJ 1000.java add

---
+++ step/java/src/boj_1000/Main.java
@@ -0,0 +1,14 @@
+package boj_1000;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        Scanner scanner = new Scanner(System.in);
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        System.out.println(a+b);
+    }
+}
