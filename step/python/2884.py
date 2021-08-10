2ba35aa9b6053ea4ff83f01b05ff99325867e25b
yuyeon329 <peachberryjr@gmail.com>
2021-08-10 23:20:06 +0900

    BOJ 2884.py add

---
+++ step/python/2884.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    h, m = map(int, input().split())
+    if m < 45:
+        if h == 0:
+            print(23, m + 15)
+        else:
+            print(h-1, m + 15)
+    else:
+        print(h, m - 45)
+
