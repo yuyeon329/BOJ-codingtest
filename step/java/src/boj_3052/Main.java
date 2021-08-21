799461dd1efd78ab785b38f488f24caef8625f64
yuyeon329 <peachberryjr@gmail.com>
2021-08-21 13:07:11 +0900

    BOJ 3052.java add

---
+++ step/java/src/boj_3052/Main.java
@@ -0,0 +1,23 @@
+package boj_3052;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[42];
+        int cnt = 0;
+        for(int i = 0; i<10;i++){
+            arr[sc.nextInt()%42]++;
+        }
+
+        for(int i = 0; i<42;i++){
+            if(arr[i]>0){
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+
+    }
+}
