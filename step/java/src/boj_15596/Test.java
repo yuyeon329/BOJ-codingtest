db19b24f73c9dd3d236b8a2ebf423ad6885104d0
yuyeon329 <peachberryjr@gmail.com>
2021-08-22 20:50:28 +0900

    BOJ 15596.java add

---
+++ step/java/src/boj_15596/Test.java
@@ -0,0 +1,11 @@
+package boj_15596;
+
+public class Test {
+    long sum(int[] a){
+        long sum = 0;
+        for(int i = 0; i< a.length;i++){
+            sum+=a[i];
+        }
+        return sum;
+    }
+}
