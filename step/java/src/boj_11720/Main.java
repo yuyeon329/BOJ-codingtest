c32c5730a736cb7348926b811e687150c1f2d9d0
yuyeon329 <peachberryjr@gmail.com>
2021-08-29 10:55:30 +0900

    BOJ 11720.java add

---
+++ step/java/src/boj_11720/Main.java
@@ -0,0 +1,16 @@
+package boj_11720;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String num = sc.next();
+        int sum = 0;
+        for(int i = 0; i<n;i++){
+            sum += num.charAt(i)-'0';
+        }
+        System.out.println(sum);
+    }
+}
