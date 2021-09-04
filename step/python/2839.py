cb2c6a3ba828a0af5107eafb03efdad12933fb46
yuyeon329 <peachberryjr@gmail.com>
2021-09-04 22:19:07 +0900

    BOJ 2839.py add

---
+++ step/python/2839.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    N = int(input())
+    bag = 0
+    while N >= 0:
+        if N%5 == 0:
+            bag += N//5
+            print(bag)
+            break
+        else:
+            N -= 3
+            bag += 1
+    else:
+        print(-1)
+
