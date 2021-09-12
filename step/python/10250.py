23bf19c08d30b567d4362f787773844dd5b64912
yuyeon329 <peachberryjr@gmail.com>
2021-09-12 12:12:53 +0900

    BOJ 10250.py update

--- step/python/10250.py
+++ step/python/10250.py
@@ -3,5 +3,5 @@ if __name__ == '__main__':
     for _ in range(T):
         H, W, N = map(int, input().split())
         floor = N % H if N % H else H
-        arc = N//H+1 if N % H else N//H
-        print('{0:01d}{1:02d}'.format(floor, arc))
+        room = N//H+1 if N % H else N//H
+        print('{0:01d}{1:02d}'.format(floor, room))
