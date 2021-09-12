a8a9effd8ec4261c05b8ab165166c91aada2d38b
yuyeon329 <peachberryjr@gmail.com>
2021-09-12 14:17:06 +0900

    BOJ 10757.java add

---
+++ step/java/src/boj_10757/Main.java
@@ -0,0 +1,13 @@
+package boj_10757;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger A = new BigInteger(sc.next());
+        BigInteger B = new BigInteger(sc.next());
+        System.out.println(A.add(B));
+    }
+}
