15a590efa6fd38b3237cf1ca787a038f28cb0ab6
yuyeon329 <peachberryjr@gmail.com>
2021-09-19 16:59:19 +0900

    BOJ 10870.py add

---
+++ step/python/10870.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    def fibo(n):
+        if n == 1:
+            return 1
+        elif n == 0:
+            return 0
+        else:
+            return fibo(n-1)+fibo(n-2)
+    n = int(input())
+    print(fibo(n))
