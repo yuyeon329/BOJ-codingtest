81eed6217048446c09ea030accbaf1a71a1cce76
yuyeon329 <peachberryjr@gmail.com>
2021-09-22 13:09:35 +0900

    BOj 11650.py add

---
+++ step/python/11650.py
@@ -0,0 +1,13 @@
+if __name__ == "__main__":
+    N = int(input())
+    point_list = []
+    for _ in range(N):
+        tmp = ()
+        tmp = tuple(map(int, input().split()))
+        point_list.append(tmp)
+
+    point_list = sorted(point_list, key=lambda x: (x[0], x[1]))
+
+    for i in point_list:
+        print(i[0], i[1])
+
