01a0c31c803add48b4e8844a5bc909424cbdb45b
yuyeon329 <peachberryjr@gmail.com>
2021-09-23 22:23:57 +0900

    BOJ 15651.py add

---
+++ step/python/15651.py
@@ -0,0 +1,15 @@
+if __name__ == "__main__":
+    N, M = map(int, input().split())
+    arr = []
+
+
+    def dfs():
+        if len(arr) == M:
+            print(" ".join(map(str, arr)))
+            return
+        for i in range(1, N+1):
+            arr.append(i)
+            dfs()
+            arr.pop()
+
+    dfs()
