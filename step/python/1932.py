3a8bb5ef87275df564d04ec940acdcedc056f462
yuyeon329 <peachberryjr@gmail.com>
2021-10-02 20:24:10 +0900

    BOj 1932.py add

---
+++ step/python/1932.py
@@ -0,0 +1,13 @@
+import sys
+if __name__ == '__main__':
+    tri = []
+    n = int(input())
+    for i in range(n):
+        tri.append(list(map(int, sys.stdin.readline().split())))
+
+    for i in range(n-2, -1, -1):
+        for j in range(i+1):
+            tri[i][j] = max(tri[i+1][j], tri[i+1][j+1]) + tri[i][j]
+
+    print(tri[0][0])
+
