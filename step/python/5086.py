fd75d92e5c7523cb89a3ac052489cb1c0648eadf
yuyeon329 <peachberryjr@gmail.com>
2021-10-10 18:49:39 +0900

    BOJ 5086.py add

---
+++ step/python/5086.py
@@ -0,0 +1,13 @@
+import sys
+if __name__ == '__main__':
+    while True:
+        a, b = map(int, sys.stdin.readline().split())
+        if a-b == 0:
+            break
+
+        if a%b == 0:
+            print("multiple")
+        elif b % a == 0:
+            print("factor")
+        else:
+            print("neither")
