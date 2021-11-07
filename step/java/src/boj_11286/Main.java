831e80ef976a1f027a9bb47e65b24dceec7b9164
yuyeon329 <peachberryjr@gmail.com>
2021-11-07 18:33:23 +0900

    BOJ 11286.java update

--- step/java/src/boj_11286/Main.java
+++ step/java/src/boj_11286/Main.java
@@ -19,15 +19,16 @@ public class Main {
         });
 
         for(int i = 0; i<N; i++){
+
             int item = Integer.parseInt(br.readLine());
+
             if(item==0){
                 if(pq.size() == 0){
                     sb.append(0).append("\n");
                 }else{
                     sb.append(pq.poll()).append("\n");
                 }
-            }
-            else{
+            }else{
                 pq.add(item);
             }
         }
